9be10c6eee98b409937907737707568747a3a38b
Rachit <gpt.rachit4@gmail.com>
2021-06-26 20:29:37 +0200

    solved cors problem

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -12,6 +12,30 @@ app.use(express.json());
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_ADDRESS);
+  
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+  
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-Requested-With, Content-Type"
+    );
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  
+    // Pass to next layer of middleware
+    next();
+  });
+
 
 app.get("/", (req, res) => res.send("App"));
 
